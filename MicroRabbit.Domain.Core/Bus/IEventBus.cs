ad4b891173cb463ca2b263d60c6abeeaa858a9b6
Nobi Hossain <nobi.hossain@selise.ch>
2021-12-29 18:51:56 +0600

    Interfaces implementaion for Microrabit.Domain.Core

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+
+        Task SendCommand<T>(T Command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+    }
+}
